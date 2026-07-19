255aae89db9dba970fd4e1db17b6f94443033fc6
JBsystems <jabwanjoe@gmail.com>
2026-07-19 19:03:22 -0400

    feat: implementing node to have fields

--- internal/raft/raft.go
+++ internal/raft/raft.go
@@ -1,10 +1,26 @@
 package raft
 
+import "sync"
+
 type NodeState int
 
 const (
-	StateIdle NodeState = iota
-	StateConnected
-	StateError
-	StateRetrying
+	Follower NodeState = iota
+	Candidate
+	Leader
 )
+
+type RaftNode struct {
+	mu sync.Mutex
+
+	nodeId int
+	peers  []string
+
+	currentTerm int
+	votedFor    int
+	log         []byte
+
+	commitIndex int
+	lastApplied int
+	state       NodeState
+}
